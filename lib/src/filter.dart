e71f4492076e746ae502326a315739770c78ca5b
Viktor Borisov <vik-borisov@users.noreply.github.com>
2025-06-11 13:14:11 +0700

    fix: stabilize tests

--- lib/src/filter.dart
+++ lib/src/filter.dart
@@ -28,7 +28,6 @@ class FilterOptions {
   late final Pointer<git_filter_options> _pointer;
 
   /// Pointer to the underlying C structure.
-  @internal
   Pointer<git_filter_options> get pointer => _pointer;
 
   /// Free memory allocated for these options.
@@ -98,7 +97,6 @@ class Filter {
   }
 
   /// Pointer to memory address for allocated filter list object.
-  @internal
   final Pointer<git_filter_list> _filterListPointer;
 
   /// Apply filter list to arbitrary [data].
